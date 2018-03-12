922e11d6fae37ae9938b355bc5c6adf301704121
Damon Khan <support@codeavengers.com>
2018-03-13 11:15:32 +1300

    Moving MinHeap into runs package

--- src/runs/MinHeap.java
+++ src/runs/MinHeap.java
@@ -4,7 +4,9 @@
 //                                                                          //
 //////////////////////////////////////////////////////////////////////////////
 
-public class MinHeap
+package runs;
+
+class MinHeap
 {
     private int[] Heap;
     private int size;
@@ -107,24 +109,5 @@ public class MinHeap
         Heap[FRONT] = Heap[size--];
         minHeapify(FRONT);
         return popped;
-    }
-
-    public static void main(String...arg)
-    {
-        System.out.println("The Min Heap is ");
-        MinHeap minHeap = new MinHeap(15);
-        minHeap.insert(5);
-        minHeap.insert(3);
-        minHeap.insert(17);
-        minHeap.insert(10);
-        minHeap.insert(84);
-        minHeap.insert(19);
-        minHeap.insert(6);
-        minHeap.insert(22);
-        minHeap.insert(9);
-        minHeap.minHeap();
-
-        minHeap.print();
-        System.out.println("The Min val is " + minHeap.remove());
     }
 }
