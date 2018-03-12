b97775446c9a1a1f45be8837686f0bd6b720cce8
Damon Khan <support@codeavengers.com>
2018-03-13 10:53:27 +1300

    Adding makeRuns file and testing arguments

---
+++ src/makeRuns.java
@@ -0,0 +1,9 @@
+public class makeRuns {
+
+    public static void main(String args[]) {
+        if (args.length != 2) {
+            System.out.println("Usage: <size> <textFile>");
+        }
+
+    }
+}
